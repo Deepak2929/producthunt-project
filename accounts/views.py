2583796999d4b302812bfcb1ac931bbebb3e16f3
deepak yadav <b1f2a3.h@gmail.com>
2018-10-06 11:08:41 +0530

    Migrations are made, Now Creating Views of Signup,Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-	return render(request, 'accounts/signup.html')
+	if request.method=="POST":
+		#Userwants to signin
+		if request.POST["password"]== request.POST["Cpassword"]:
+			try:
+				user = User.objects.get(username=request.POST['username'])
+				return render(request, 'accounts/signup.html',{'error':'Username allready Exist.Try new one!'})
+			except User.DoesNotExist:
+				user = User.objects.create_user(request.POST['username'],password=request.POST["Cpassword"])
+				auth.login(request,user)
+				return redirect('home')
+			
+	else:
+		
+		return render(request, 'accounts/signup.html')
 
 	
 	
